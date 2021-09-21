0e1f2e6b02536e48eb9cc7797441b2af60a0cab0
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-09-21 15:03:37 -0400

    wip

--- src/Components/About.js
+++ src/Components/About.js
@@ -32,7 +32,7 @@ class About extends Component {
             <p>{bio}</p>
             <div className="row">
               <div className="columns contact-details">
-                <h2>Contact Details</h2>
+                <h2>Contact Information</h2>
                 <p className="address">
                   <span>{name}</span>
                   <br />
