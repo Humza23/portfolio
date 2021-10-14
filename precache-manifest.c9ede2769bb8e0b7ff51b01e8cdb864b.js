a2c1775fcefd25c20ace5df36a8072ee54cff256
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-10-14 15:44:43 -0400

    Updates

---
+++ precache-manifest.c9ede2769bb8e0b7ff51b01e8cdb864b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ab04efc863332005522d5a58c53901",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "083d765ce776099326e7",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "50b2a552b16ed3f44700",
+    "url": "/portfolio/static/js/2.db3644e9.chunk.js"
+  },
+  {
+    "revision": "52424457e5c4fa93c31028243ad4df2b",
+    "url": "/portfolio/static/js/2.db3644e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083d765ce776099326e7",
+    "url": "/portfolio/static/js/main.84f22a28.chunk.js"
+  },
+  {
+    "revision": "d6ca8614ab4df5b1bf68",
+    "url": "/portfolio/static/js/runtime-main.44645aae.js"
+  },
+  {
+    "revision": "6c79274895aff8d30de4d69457cfd831",
+    "url": "/portfolio/static/media/2021HumzaResume.6c792748.pdf"
+  },
+  {
+    "revision": "3359119defc88a608e351bf75e18efc8",
+    "url": "/portfolio/static/media/logoCSS.3359119d.svg"
+  },
+  {
+    "revision": "652c9dfb2171ba14eb9ee9c0217d5c91",
+    "url": "/portfolio/static/media/logoExpress.652c9dfb.svg"
+  },
+  {
+    "revision": "adcab6a564a11096dc35f681cfb02ce6",
+    "url": "/portfolio/static/media/logoGit.adcab6a5.svg"
+  },
+  {
+    "revision": "cba10c7c1ce484b82ab9b156b9abc74a",
+    "url": "/portfolio/static/media/logoHTML.cba10c7c.svg"
+  },
+  {
+    "revision": "0c1f700da144243c526f252e59362138",
+    "url": "/portfolio/static/media/logoJS.0c1f700d.svg"
+  },
+  {
+    "revision": "4d315923fdb4ed913816974c5cdd8004",
+    "url": "/portfolio/static/media/logoNode.4d315923.svg"
+  },
+  {
+    "revision": "14ade307b848d2654013f9f24c99b807",
+    "url": "/portfolio/static/media/logoPostgreSQL.14ade307.svg"
+  },
+  {
+    "revision": "d4eebc6dc7a839b5052f793919082d4d",
+    "url": "/portfolio/static/media/logoReact.d4eebc6d.svg"
+  },
+  {
+    "revision": "332417a7f4156fbd6f81615a48286092",
+    "url": "/portfolio/static/media/logoSQLite.332417a7.svg"
+  },
+  {
+    "revision": "11e6b6b32fcf882439484551d9907a92",
+    "url": "/portfolio/static/media/profilepic.11e6b6b3.jpg"
+  },
+  {
+    "revision": "85278f83bbff4b23d9d29d799696332b",
+    "url": "/portfolio/static/media/project1.85278f83.jpg"
+  },
+  {
+    "revision": "dbb8cca697117b7303080be2cc4301f7",
+    "url": "/portfolio/static/media/project2.dbb8cca6.jpg"
+  },
+  {
+    "revision": "58e01e441e86a1057aacbd1a46142a24",
+    "url": "/portfolio/static/media/project3.58e01e44.jpg"
+  },
+  {
+    "revision": "2a177ee80e54012bd3bb8bdbe0637cd4",
+    "url": "/portfolio/static/media/project4.2a177ee8.jpg"
+  },
+  {
+    "revision": "6498890585dee1db1dcf10f1fcd0c7c0",
+    "url": "/portfolio/static/media/project5.64988905.jpg"
+  }
+]);
