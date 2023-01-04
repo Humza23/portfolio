d4056c6bb5edfe7774161cecd21de576a8210670
Humza Malik <humza718@gmail.com>
2023-01-04 15:02:00 -0500

    Updates

---
+++ precache-manifest.01d15750cc573021c7b4f3d6fe20b0e8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf4de31559dbc65a905fdef9a3cab4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4442679fbf6b4b80de",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "fb7b5c4a29774f95cc3e",
+    "url": "/static/js/2.e02ab5c6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e02ab5c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4442679fbf6b4b80de",
+    "url": "/static/js/main.5fc792c5.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  },
+  {
+    "revision": "9edc101d3086e506051c75e544451aed",
+    "url": "/static/media/HumzaMalikResume.9edc101d.pdf"
+  },
+  {
+    "revision": "3359119defc88a608e351bf75e18efc8",
+    "url": "/static/media/logoCSS.3359119d.svg"
+  },
+  {
+    "revision": "652c9dfb2171ba14eb9ee9c0217d5c91",
+    "url": "/static/media/logoExpress.652c9dfb.svg"
+  },
+  {
+    "revision": "adcab6a564a11096dc35f681cfb02ce6",
+    "url": "/static/media/logoGit.adcab6a5.svg"
+  },
+  {
+    "revision": "cba10c7c1ce484b82ab9b156b9abc74a",
+    "url": "/static/media/logoHTML.cba10c7c.svg"
+  },
+  {
+    "revision": "0c1f700da144243c526f252e59362138",
+    "url": "/static/media/logoJS.0c1f700d.svg"
+  },
+  {
+    "revision": "4d315923fdb4ed913816974c5cdd8004",
+    "url": "/static/media/logoNode.4d315923.svg"
+  },
+  {
+    "revision": "14ade307b848d2654013f9f24c99b807",
+    "url": "/static/media/logoPostgreSQL.14ade307.svg"
+  },
+  {
+    "revision": "71374043d9b3d9ea7fa09976a747d475",
+    "url": "/static/media/logoPython.71374043.svg"
+  },
+  {
+    "revision": "d4eebc6dc7a839b5052f793919082d4d",
+    "url": "/static/media/logoReact.d4eebc6d.svg"
+  },
+  {
+    "revision": "332417a7f4156fbd6f81615a48286092",
+    "url": "/static/media/logoSQLite.332417a7.svg"
+  },
+  {
+    "revision": "20c1535fc95c572afc699b0081403bba",
+    "url": "/static/media/profilepic.20c1535f.jpg"
+  },
+  {
+    "revision": "724b41d24d6c7be1de8ecbbb869116d8",
+    "url": "/static/media/projectBeachResort.724b41d2.png"
+  },
+  {
+    "revision": "3c96897a26b3fc1ca7ea927bab5ce9cc",
+    "url": "/static/media/projectBusinessPageTemplate.3c96897a.png"
+  },
+  {
+    "revision": "e1ffc28502cde66018c324aa6c98b549",
+    "url": "/static/media/projectCryptoTracker.e1ffc285.png"
+  },
+  {
+    "revision": "e695f4c587d99f96c870eede16ec3f38",
+    "url": "/static/media/projectEcommercePlants.e695f4c5.png"
+  },
+  {
+    "revision": "6c2924109808c527c3937e76d0d4f71f",
+    "url": "/static/media/projectHRFAsylum.6c292410.png"
+  },
+  {
+    "revision": "4a4f8aba1d7449c7cec3ab76d1926c60",
+    "url": "/static/media/projectWeather.4a4f8aba.png"
+  }
+]);
