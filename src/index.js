f80fde30380739bd8fecda8aba8398845c438751
Humza Malik <humza718@gmail.com>
2023-01-04 15:01:45 -0500

    updated

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+console.log('nothing');
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
